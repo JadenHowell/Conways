971b5ad51cf0caf4d38ee39d12aa67655463419f
JadenHowell <speedingj@gmail.com>
2020-12-19 12:13:52 -0700

    Got basic resizing of lower panel done. Still needs fine tuning.

--- src/guicomponents/GUI.java
+++ src/guicomponents/GUI.java
@@ -14,11 +14,19 @@ public class GUI {
         frame.setLayout(new BorderLayout());
 
         mainPanel = new MainPanel(screenSize);
-        frame.add(mainPanel, BorderLayout.CENTER);
-
-
+        //frame.add(mainPanel, BorderLayout.CENTER);
         lowerPanel = new LowerPanel(mainPanel);
-        frame.add(lowerPanel, BorderLayout.SOUTH);
+        //frame.add(lowerPanel, BorderLayout.SOUTH);
+
+        JSplitPane splitPane = new JSplitPane();
+        frame.getContentPane().add(splitPane);
+        splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        splitPane.setDividerLocation((int)screenSize.getHeight()-200);
+        splitPane.setResizeWeight(.9);
+        splitPane.setTopComponent(mainPanel);
+        splitPane.setBottomComponent(lowerPanel);
+        splitPane.setEnabled(false);
+
 
         frame.setLocationRelativeTo(null); //This centers it in the screen for some reason
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
